5e22befcf6c51a630960b1c30c28aa79aaed2b34
Trương Bình Khang <khang.truongbinh@gmail.com>
2018-07-21 00:21:27 +0700

    Anfis + SA code

--- prediction/anf_py/models.py
+++ prediction/anf_py/models.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 from sklearn.metrics import mean_absolute_error
-
-
+from random import uniform as random
+from scipy.constants import Boltzmann
 def premise_parameter(para_shape, min_mu=10.0, max_mu=15.0, min_sigma=5.0, max_sigma=10.0):
     """
 
@@ -25,6 +25,11 @@ def consequence_parameter(para_shape):
     para_init = tf.random_normal(para_shape)
     return tf.Variable(para_init)
 
+# Lay mot lan can cua ma tran chua tham so
+def neighbor(x):
+    delta = tf.random_normal(shape=x.get_shape(), mean=0.0, stddev=0.001*tf.reduce_mean(x))
+    x = x + delta
+    return x
 
 class ANFIS:
     def __init__(self, rule_number, window_size):
@@ -136,6 +141,83 @@ class ANFIS:
         net.close()
         # duration = time.time() - start_time
         # print(duration)
+
+    def hybridSATraining(self, x_train, y_train, x_test, y_test, batch_size, epoch,
+                         rate, temp_init, neighbor_number, reduce_factor):
+        # Session
+        net = tf.InteractiveSession()
+        # Placeholder
+        x = tf.placeholder(dtype=tf.float32, shape=[None, 1, self.window_size])
+        y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+
+        # Cost function
+        cost = tf.reduce_mean(tf.squared_difference(self.predict(x, batch_size), y))
+
+        # Entire train loss function
+        # Phan code nay dung de lay cac gia tri cua train loss, su dung de ve~ do thi hoi tu loss
+        # lost = tf.reduce_mean(tf.squared_difference(self.predict(x, x_train.shape[0]), y))
+        # lost_list = []
+        f_tensor = tf.reduce_mean(tf.squared_difference(self.predict(x, x_train.shape[0]), y))
+
+        # Optimizer
+        optimizer = tf.train.AdamOptimizer(rate).minimize(cost)
+
+        # Test loss
+        # acc la bien dua ra gia tri ve RMSE cua tap test so voi gia tri du doan duoc
+        acc = tf.sqrt(tf.reduce_mean(tf.squared_difference(self.predict(x, x_test.shape[0]), y)))
+        # Su dung pred nay de dua ra duoc MAE vi MAE khong co san trong tensorflow
+        # Viec nay khong anh huong nhieu den code performance nen khong can dua het vao tensor cung duoc
+        pred = self.predict(x, x_test.shape[0])
+
+        # Init session
+        net.run(tf.global_variables_initializer())
+
+        # Start training
+        for e in range(epoch):
+            # Shuffle training data
+            shuffle = np.random.permutation(np.arange(len(y_train)))
+            x_train = x_train[shuffle]
+            y_train = y_train[shuffle]
+
+            # Based-batch training
+            for i in np.arange(0, len(y_train) // batch_size):
+                start = i * batch_size
+                batch_x = x_train[start:start + batch_size]
+                batch_y = y_train[start:start + batch_size]
+
+                # Optimizing
+                net.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+            # SA training ngay sau do
+            previous_parameters = self.w_fuzz, self.weights, self.bias
+            temp = temp_init
+            f0 = net.run(f_tensor)
+            for n in range(neighbor_number):
+                sess.run(self.w_fuzz.assign(neighbor(self.w_fuzz)))
+                sess.run(self.weights.assign(neighbor(self.weights)))
+                sess.run(self.bias.assign(neighbor(self.bias)))
+                f = net.run(f_tensor)
+
+                if (f < f0):
+                    f_new = f
+                else:
+                    df = f - f0
+                    r = random(0, 1)
+                    if r > exp(-df/Boltzmann/temp):
+                        f_new = f
+                    else:
+                        f_new = f0
+                        self.w_fuzz, self.weights, self.bias = previous_parameters
+                f0 = f_new
+                temp = reduce_factor * temp
+            test_rmse = net.run(acc, feed_dict={x: x_test, y: y_test})  # RMSE test
+            pred_value = net.run(pred, feed_dict={x: x_test})
+            test_mae = mean_absolute_error(pred_value, y_test)  # MAE test
+            print(e, 'Test-RMSE: ', test_rmse, 'MAE:', test_mae)
+
+        net.close()
+        # duration = time.time() - start_time
+        # print(duration)
+
 
     def summary(self):
         pass
