ea835c3d4d028f82b53e4108442fd92b2dfc1475
khangtruongbinh <khang.truongbinh@gmail.com>
2018-09-15 08:36:12 +0700

    update models

--- prediction/anf_py/remodel.py
+++ prediction/anf_py/remodel.py
@@ -19,8 +19,9 @@ def save_figures(data, label,
 
 
 # Initialize variables as Premise Parameter
-def premise_parameter(
-        para_shape, min_mu=10.0, max_mu=15.0, min_sigma=5.0, max_sigma=10.0):
+def premise_parameter(para_shape,
+                      min_mu=10.0, max_mu=15.0,
+                      min_sigma=5.0, max_sigma=10.0):
     """
 
     :param para_shape:
@@ -43,8 +44,7 @@ def writer(context):
 
 
 # Initialize variables as Consequence Parameter
-def consequence_parameter(
-        para_shape):
+def consequence_parameter(para_shape):
     para_init = tf.random_normal(para_shape)
     return tf.Variable(para_init)
 
@@ -64,7 +64,7 @@ class ANFIS:
         self.w_fuzz = premise_parameter(self.premise_shape)
         self.weights = consequence_parameter(self.consequence_shape_weights)
         self.bias = consequence_parameter(self.consequence_shape_bias)
-
+    
     def output(self, x: np.ndarray):
         """
         Show list of outputs from list of inputs
@@ -74,7 +74,7 @@ class ANFIS:
         # Reshape
         with tf.name_scope("reshape"):
             x_input = tf.tile(x, [1, self.rule_number, 1])
-
+        
         # Fuzzification Layer
         with tf.name_scope('layer_1'):
             fuzzy_sets = tf.exp(- tf.divide(tf.square(tf.subtract(x_input, self.w_fuzz['mu']) / 2.0),
@@ -82,20 +82,19 @@ class ANFIS:
         # Rule-set Layer
         with tf.name_scope('layer_2'):
             fuzzy_rules = tf.reduce_prod(fuzzy_sets, axis=2)
-
+        
         # Normalization Layer
         with tf.name_scope('layer_3'):
             sum_fuzzy_rules = tf.reduce_sum(fuzzy_rules, axis=1)
             normalized_fuzzy_rules = tf.divide(fuzzy_rules, tf.reshape(sum_fuzzy_rules, (-1, 1)))
-
+        
         # Defuzzification Layer and Output Layer
         with tf.name_scope('layer_4_5'):
             f = tf.add(tf.matmul(tf.reshape(x, (-1, self.window_size)), self.weights), self.bias)
             output = tf.reduce_sum(tf.multiply(normalized_fuzzy_rules, f), axis=1)
-
+        
         return tf.reshape(output, (-1, 1))
-
-
+    
     # Traning ANFIS model
     def train(self,
               x_train, y_train,
@@ -118,24 +117,24 @@ class ANFIS:
         writer("Creating Placeholder ... ")
         x = tf.placeholder(dtype=tf.float32, shape=[None, 1, self.window_size])
         y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
-
+        
         # Creating cost and optimizer
         writer("Creating cost and optimizer")
         cost = tf.reduce_mean(tf.squared_difference(self.output(x), y))
         optimizer = tf.train.AdamOptimizer(learning_rate=rate).minimize(cost)
-
+        
         saver = tf.train.Saver()
-
+        
         # Check tracking_loss flags
         track_list = np.empty((0,))
-
+        
         # Initializing session
         with tf.Session() as sess:
-
+            
             # Check Model path Loading
             if load_path is not None:
                 saver.restore(sess, load_path)
-
+            
             # Start training
             sess.run(tf.global_variables_initializer())
             writer("Starting train ... ")
@@ -143,22 +142,22 @@ class ANFIS:
                 sess.run(optimizer, feed_dict={x: x_train, y: y_train})
                 c = sess.run(cost, feed_dict={x: x_train, y: y_train})
                 writer(f"{e}: {c}")
-
+                
                 # Appened new loss value to track_list
                 if tracking_loss:
                     track_list = np.append(track_list, c)
-
+            
             # Check save_path
             if save_path is not None:
                 saver.save(sess, save_path)
-
+        
         # Saving figures
         fig_path = f"{save_path}_tracking.png"
         writer(f"Saving tracking figures to {fig_path} ")
         save_figures(data=track_list, label="Loss_function",
                      x_label='epoch', y_label='loss value',
                      path=fig_path)
-
+    
     # Compute loss from input data and compare output with labels
     def loss(self,
              x, y,
@@ -173,9 +172,9 @@ class ANFIS:
         x_ = tf.placeholder(dtype=tf.float32, shape=[None, 1, self.window_size])
         y_ = tf.placeholder(dtype=tf.float32, shape=[None, 1])
         cost = tf.reduce_mean(tf.squared_difference(self.output(x_), y_))
-
+        
         saver = tf.train.Saver()
-
+        
         with tf.Session() as sess:
             # Check Model path Loading
             if load_path is not None:
